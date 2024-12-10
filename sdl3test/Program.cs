695f0dad11865c7b68398af3f314518c95554a4b
DaveAstator <omnispark@gmail.com>
2024-12-10 20:10:46 +0100

    rect test

--- sdl3test/Program.cs
+++ sdl3test/Program.cs
@@ -13,14 +13,20 @@ class Program
 
             SDL_Init((SDL_InitFlags)SDL_INIT_VIDEO);
 
-            var win = SDL_CreateWindow("Hello World", width, height, 0);
+            var win = SDL_CreateWindow("Hello World", width, height, SDL_WindowFlags.SDL_WINDOW_RESIZABLE);
 
             var renderer = SDL_CreateRenderer(win,(byte*)null);
 
             var bitmapSurface = SDL_LoadBMP("C:\\Eugene\\ctron.bmp");
             var bitmapTex = SDL_CreateTextureFromSurface(renderer, bitmapSurface);
             SDL_DestroySurface(bitmapSurface);
-
+            var tgt_rect = new SDL_FRect
+            {
+                x = 40,
+                y = 60,
+                w = 300,
+                h = 300
+            };
             while (!loopShouldStop)
             {
                 unsafe
@@ -37,7 +43,7 @@ class Program
                     }
 
                     SDL_RenderClear(renderer);
-                    SDL_RenderTexture(renderer, bitmapTex, (SDL_FRect*)IntPtr.Zero, (SDL_FRect*)IntPtr.Zero);
+                    SDL_RenderTexture(renderer, bitmapTex, (SDL_FRect*)IntPtr.Zero, &tgt_rect);
                     SDL_RenderPresent(renderer);
                 }
             }
